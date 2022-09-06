3f40f5f42297a31ef55be3cf89b542e69f427251
login <longjin@ringotek.cn>
2022-09-06 19:57:35 +0800

    new srs&video controller (#6)

--- server/app.py
+++ server/app.py
@@ -1,14 +1,19 @@
 from fastapi import FastAPI, APIRouter
 from controller.HelloWorldController import HelloWorldController
-
+from controller.SrsController import SRSController
+from controller.VideoController import VideoController
 
 app = FastAPI()
 
-# 初始化HelloWorldController实例
-helloworld_controller = HelloWorldController(APIRouter(prefix="/helloworld", tags=["custom"]))
-# 将HelloWorldController中的路由注册到app中
-app.include_router(helloworld_controller.router)
+# 初始化Controller实例
+helloworld_controller = HelloWorldController(APIRouter(prefix="/api/v1/helloworld", tags=["custom"]))
+srs_controller = SRSController(APIRouter(prefix="/api/v1/srs", tags=["custom"]))
+video_controller = VideoController(APIRouter(prefix="/api/v1/video", tags=["custom"]))
 
+# 将Controller中的路由注册到app中
+app.include_router(helloworld_controller.router)
+app.include_router(srs_controller.router)
+app.include_router(video_controller.router)
 
 @app.get("/")
 async def root():
---
+++ server/controller/VideoController.py
@@ -0,0 +1,10 @@
+from fastapi import APIRouter
+
+
+class VideoController:
+    def __init__(self, router: APIRouter):
+        self.router = router
+        self.router.add_api_route("/", self.GET, methods=["GET"])
+
+    def GET(self):
+        return {"message": "Hello World"}
